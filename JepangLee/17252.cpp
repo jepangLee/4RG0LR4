4ba3c4030c812816ea98c74d2e771deca67206b2
JepangLee <ljsung0805@gmail.com>
2019-06-16 22:23:26 +0900

    Upload gitignore and code at 3rd and 4th week of June

---
+++ JepangLee/17252.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <math.h>
+int main()
+{
+	int in, check = 0, de = pow(3, 17);
+	std::cin >> in;
+	if (in <= 0) {
+		printf("NO");
+		return 0;
+	}
+	for ( ; de > 0; de /= 3) {
+		if (in - de >= 0) in -= de;
+		if (0 == in) {
+			printf("YES \n");
+			return 0;
+		}
+	}
+	printf("NO\n");
+}
